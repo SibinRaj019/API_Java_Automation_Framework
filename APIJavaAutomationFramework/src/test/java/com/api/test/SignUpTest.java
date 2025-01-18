e47b23d1d50ecb7dbea4e4829a3c50d27c8957e1
SibinRaj019 <119923460+SibinRaj019@users.noreply.github.com>
2025-01-18 10:10:14 +0530

    Auth Service: SignUp Test

---
+++ APIJavaAutomationFramework/src/test/java/com/api/test/SignUpTest.java
@@ -0,0 +1,27 @@
+package com.api.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.api.base.AuthenticationService;
+import com.api.models.request.SignUpRequestBody;
+
+import io.restassured.response.Response;
+
+public class SignUpTest {
+	@Test(description = "Verify create account test")
+	public static void signUp() {
+		AuthenticationService authenticationService = new AuthenticationService();
+		SignUpRequestBody signUpRequestBody = new SignUpRequestBody.Builder()
+				.username("basha124")
+				.email("bashaManik@hotmail.com")
+				.firstName("Basha")
+				.password("Manik123")
+				.lastName("Manik")
+				.mobileNumber("9717171718")
+				.build();	
+		Response response = authenticationService.signUp(signUpRequestBody);
+		System.out.println(response.asPrettyString());
+		Assert.assertEquals(response.asPrettyString(),"User registered sucessfully!");
+	}
+}
