47239d12a549307cc06c0668e4223e8847f439fa
SibinRaj019 <119923460+SibinRaj019@users.noreply.github.com>
2025-01-18 10:24:58 +0530

    Adding Filters

--- APIJavaAutomationFramework/src/test/java/com/api/listeners/TestListeners.java
+++ APIJavaAutomationFramework/src/test/java/com/api/listeners/TestListeners.java
@@ -7,44 +7,44 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListeners implements ITestListener {
-	
-    private static final Logger logger = LogManager.getLogger(TestListeners.class);
-        
-    @Override
-    public void onStart(ITestContext context) {
-    	logger.info("Test Suite started: " + context.getName());
-    }
 
-    @Override
-    public void onFinish(ITestContext context) {
-        logger.info("Test Suite finished: " + context.getName());
-    }
+	private static final Logger logger = LogManager.getLogger(TestListeners.class);
 
-    @Override
-    public void onTestStart(ITestResult result) {
-        logger.info("Test started: " + result.getName());
-    }
+	@Override
+	public void onStart(ITestContext context) {
+		logger.info("Test Suite started: " + context.getName());
+	}
 
-    @Override
-    public void onTestSuccess(ITestResult result) {
-        logger.info("Test passed: " + result.getMethod().getMethodName());
-        logger.info("Test Description: " + result.getMethod().getDescription());
-    }
+	@Override
+	public void onFinish(ITestContext context) {
+		logger.info("Test Suite finished: " + context.getName());
+	}
 
-    @Override
-    public void onTestFailure(ITestResult result) {
-        logger.error("Test failed: " + result.getName());
-        logger.error("Reason: " + result.getThrowable());
-    }
+	@Override
+	public void onTestStart(ITestResult result) {
+		logger.info("Test started: " + result.getName());
+	}
 
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        logger.info("Test skipped: " + result.getName());
-    }
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		logger.info("Test passed: " + result.getMethod().getMethodName());
+		logger.info("Test Description: " + result.getMethod().getDescription());
+	}
 
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        logger.info("Test failed but within success percentage: " + result.getName());
-    }
-    
+	@Override
+	public void onTestFailure(ITestResult result) {
+		logger.error("Test failed: " + result.getName());
+		logger.error("Reason: " + result.getThrowable());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		logger.info("Test skipped: " + result.getName());
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		logger.info("Test failed but within success percentage: " + result.getName());
+	}
+
 }
