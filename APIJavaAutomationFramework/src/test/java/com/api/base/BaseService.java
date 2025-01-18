f3e98bd7edb1644bcf49728bb8d25c5ad52077b4
SibinRaj019 <119923460+SibinRaj019@users.noreply.github.com>
2025-01-18 09:36:38 +0530

    Base service and Pom dependencies

---
+++ APIJavaAutomationFramework/src/test/java/com/api/base/BaseService.java
@@ -0,0 +1,64 @@
+package com.api.base;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseService {
+	/**
+	 * Class contains common properties and method
+	 */
+	private static final String BASE_URI = "http://64.227.160.186:8080";
+	private RequestSpecification requestSpecification;
+
+	/**
+	 * Constructor is used to Initialize - Instance variable
+	 */
+	public BaseService() {
+		requestSpecification = given().baseUri(BASE_URI);
+	}
+
+	/**
+	 * How to attach authentication token
+	 * 
+	 * @param token
+	 */
+	protected void setAuthToken(String token) {
+		requestSpecification.header("Authorization", "Bearer " + token);
+	}
+
+	/**
+	 * How to make post request
+	 * 
+	 * @param payload
+	 * @param endpoint
+	 * @return
+	 */
+	protected Response PostRequest(Object payload, String endpoint) {
+		return requestSpecification.contentType(ContentType.JSON).body(payload).post(endpoint);
+	}
+
+	/**
+	 * How to make get request
+	 * 
+	 * @param endpoint
+	 * @return
+	 */
+	protected Response getRequest(String endpoint) {
+		return requestSpecification.get(endpoint);
+	}
+
+	/**
+	 * How to make put request
+	 * 
+	 * @param payload
+	 * @param endpoint
+	 * @return
+	 */
+	protected Response PutRequest(Object payload, String endpoint) {
+		System.out.println(payload.toString());
+		return requestSpecification.contentType(ContentType.JSON).body(payload).put(endpoint);
+	}
+}
